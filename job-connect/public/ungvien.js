d6e93c6eb7866a198f3126af725f68a0545cb2af
cuongus18 <cuongungsy@gmail.com>
2019-03-26 11:22:04 +0700

    job-connect

---
+++ job-connect/public/ungvien.js
@@ -0,0 +1,35 @@
+var info_ungvien = [
+    {
+        ho_ten: "Nguyễn Thanh Chung",
+        trinh_do: "Đại học",
+        truong: "Đại học Công nghiệp",
+        chuyen_nganh: "Kỹ sư Tự động hóa nhiệt",
+        nam_kinhnghiem: "2 năm",
+        sdt: "979043212"
+    },
+    {
+        ho_ten: "Nguyễn Thanh Thảo",
+        trinh_do: "Đại học",
+        truong: "Đại học Kinh tế quốc dân",
+        chuyen_nganh: "Kế toán",
+        nam_kinhnghiem: "2 năm",
+        sdt: "979112134"
+    },
+    {
+        ho_ten: "Hoàng Bá Bình",
+        trinh_do: "Đại học",
+        truong: "Đại học Xây dựng",
+        chuyen_nganh: "Kỹ thuật cầu đường",
+        nam_kinhnghiem: "3 năm",
+        sdt: "979112134"
+    },
+    {
+        ho_ten: "Đỗ Văn Bình",
+        trinh_do: "Cao đẳng",
+        truong: "Cao đẳng FPT BTEC",
+        chuyen_nganh: "Lập trình viên",
+        nam_kinhnghiem: "1 năm",
+        sdt: "979112134"
+    }
+
+];
